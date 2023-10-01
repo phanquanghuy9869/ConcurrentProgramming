d8dc7373b009bd4ffe81e41259137507f6643d64
Huy Phan Quang <huy.phan.quang@viedoc.com>
2023-10-01 21:59:54 +0700

    Initial commit

---
+++ 01.LockBasic/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.LockBasic
+{
+    internal class Program
+    {
+        public const string FileLocation = "C:\\Homespace\\TestFiles\\20231001";
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    File.AppendAllText(FileLocation, $"Write from task {i}");
+                    Thread.Sleep(1000);
+                    File.AppendAllText(FileLocation, $"Write from task {i} second times");
+                });
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
